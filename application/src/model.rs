23b0ce887e978bd7dc0e8d4658ac06b3b12e2dc6
Thomas Bratt <thomas.bratt@gmail.com>
2020-06-03 20:36:20 +0100

    Add velocity to shape and revert world clipping in preparation for move general collision detection

--- application/src/model.rs
+++ application/src/model.rs
@@ -3,8 +3,8 @@ use crate::shape;
 
 use framework::Action;
 use math::clamp;
+use shape::is_intersection;
 use shape::Shape;
-use std::cmp;
 
 // TODO: should be part of model? How will window resize work?
 pub const WORLD_WIDTH: u32 = 1600;
@@ -21,18 +21,11 @@ const BALL_MOVE_INCREMENT: i32 = 8;
 pub struct Model {
     pub bat: Shape,
     pub ball: Shape,
-    pub ball_dx: i32,
-    pub ball_dy: i32,
 }
 
 impl Model {
-    pub fn new(bat: Shape, ball: Shape, ball_dx: i32, ball_dy: i32) -> Self {
-        Model {
-            bat,
-            ball,
-            ball_dx,
-            ball_dy,
-        }
+    pub fn new(bat: Shape, ball: Shape) -> Self {
+        Model { bat, ball }
     }
 }
 
@@ -43,51 +36,37 @@ pub fn initialize() -> Model {
             (WORLD_HEIGHT - 2 * BAT_HEIGHT) as i32,
             BAT_WIDTH,
             BAT_HEIGHT,
+            0,
+            0,
         ),
         Shape::new(
             (WORLD_WIDTH / 2 - BALL_RADIUS) as i32,
             BALL_RADIUS as i32,
             2 * BALL_RADIUS,
             2 * BALL_RADIUS,
+            0,
+            BALL_MOVE_INCREMENT,
         ),
-        0,
-        BALL_MOVE_INCREMENT,
     )
 }
 
 pub fn update(action: Action, original: &Model) -> Option<Model> {
-    let new_bat_dx = match action {
-        Action::Left => -BAT_MOVE_INCREMENT,
-        Action::Right => BAT_MOVE_INCREMENT,
-        _ => 0,
+    let new_bat = match action {
+        Action::Left => original.bat.velocity(-BAT_MOVE_INCREMENT, 0).move_step(),
+        Action::Right => original.bat.velocity(BAT_MOVE_INCREMENT, 0).move_step(),
+        _ => original.bat,
     };
 
     let is_hit = is_intersection(&original.bat, &original.ball);
 
-    let new_ball_dx = match action {
-        a if a == Action::Left && is_hit => -BALL_MOVE_INCREMENT,
-        a if a == Action::Right && is_hit => BALL_MOVE_INCREMENT,
-        _ => original.ball_dx,
+    let (new_ball_dx, new_ball_dy) = match action {
+        a if a == Action::Left && is_hit => (-BALL_MOVE_INCREMENT, -BALL_MOVE_INCREMENT),
+        a if a == Action::Right && is_hit => (BALL_MOVE_INCREMENT, -BALL_MOVE_INCREMENT),
+        _ => (original.ball.dx, original.ball.dy),
     };
+    let new_ball = original.ball.velocity(new_ball_dx, new_ball_dy).move_step();
 
-    let new_ball_dy = if is_hit {
-        -BALL_MOVE_INCREMENT
-    } else {
-        original.ball_dy
-    };
-
-    Some(Model::new(
-        move_center_position(&original.bat, new_bat_dx, 0, WORLD_WIDTH, WORLD_HEIGHT),
-        move_center_position(
-            &original.ball,
-            new_ball_dx,
-            new_ball_dy,
-            WORLD_WIDTH,
-            WORLD_HEIGHT,
-        ),
-        new_ball_dx,
-        new_ball_dy,
-    ))
+    Some(Model::new(new_bat, new_ball))
 
     // TODO: check for miss when ball goes off screen
 }
@@ -96,28 +75,25 @@ pub fn quit(model: Model) {
     println!("Quit. Save model etc: {:?}", model);
 }
 
-fn move_center_position(
-    original: &Shape,
-    dx: i32,
-    dy: i32,
-    world_width: u32,
-    world_height: u32,
-) -> Shape {
-    let x_min: i32 = (original.width / 2) as i32;
-    let x_max: i32 = (world_width - (original.width / 2)) as i32;
-
-    let y_min: i32 = (original.height / 2) as i32;
-    let y_max: i32 = (world_height - (original.height / 2)) as i32;
-
-    Shape::new(
-        clamp(original.x + dx, x_min, x_max),
-        clamp(original.y + dy, y_min, y_max),
-        original.width,
-        original.height,
-    )
-}
-
-fn is_intersection(s1: &Shape, s2: &Shape) -> bool {
-    cmp::max(s1.x - s2.x, s2.x - s1.x) < (s1.width / 2 + s2.width / 2) as i32
-        && cmp::max(s1.y - s2.y, s2.y - s1.y) < (s1.height / 2 + s2.height / 2) as i32
-}
+// fn move_center_position(
+//     original: &Shape,
+//     dx: i32,
+//     dy: i32,
+//     world_width: u32,
+//     world_height: u32,
+// ) -> Shape {
+//     let x_min: i32 = (original.width / 2) as i32;
+//     let x_max: i32 = (world_width - (original.width / 2)) as i32;
+//
+//     let y_min: i32 = (original.height / 2) as i32;
+//     let y_max: i32 = (world_height - (original.height / 2)) as i32;
+//
+//     Shape::new(
+//         clamp(original.x + dx, x_min, x_max),
+//         clamp(original.y + dy, y_min, y_max),
+//         original.width,
+//         original.height,
+//         original.dx,
+//         original.dy
+//     )
+// }
--- application/src/shape.rs
+++ application/src/shape.rs
@@ -1,31 +1,48 @@
-#[derive(Debug)]
+use std::cmp;
+
+#[derive(Copy, Clone, Debug)]
 pub struct Shape {
     pub x: i32,
     pub y: i32,
     pub width: u32,
     pub height: u32,
+    pub dx: i32,
+    pub dy: i32,
 }
 
 impl Shape {
-    pub fn new(x: i32, y: i32, width: u32, height: u32) -> Self {
+    pub fn new(x: i32, y: i32, width: u32, height: u32, dx: i32, dy: i32) -> Self {
         Shape {
             x,
             y,
             width,
             height,
+            dx,
+            dy,
         }
     }
 
-    pub fn make_copy(original: &Shape) -> Self {
-        let x = original.x;
-        let y = original.y;
-        let width = original.width;
-        let height = original.height;
-        Shape {
-            x,
-            y,
-            width,
-            height,
-        }
+    pub fn velocity(self: &Self, new_dx: i32, new_dy: i32) -> Self {
+        Shape::new(self.x, self.y, self.width, self.height, new_dx, new_dy)
     }
+
+    pub fn move_to(self: &Self, new_x: i32, new_y: i32) -> Self {
+        Shape::new(new_x, new_y, self.width, self.height, self.dx, self.dy)
+    }
+
+    pub fn move_step(self: &Self) -> Self {
+        Shape::new(
+            self.x + self.dx,
+            self.y + self.dy,
+            self.width,
+            self.height,
+            self.dx,
+            self.dy,
+        )
+    }
+}
+
+pub(crate) fn is_intersection(s1: &Shape, s2: &Shape) -> bool {
+    cmp::max(s1.x - s2.x, s2.x - s1.x) < (s1.width / 2 + s2.width / 2) as i32
+        && cmp::max(s1.y - s2.y, s2.y - s1.y) < (s1.height / 2 + s2.height / 2) as i32
 }
